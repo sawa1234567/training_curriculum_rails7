fcd9bde72b92a960e10ba16162a47fa60a96a082
sawa1234567 <ss.sawa80@gmail.com>
2024-04-12 22:44:48 +0900

    issue1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -31,10 +31,18 @@ class CalendarsController < ApplicationController
 
     7.times do |x|
       today_plans = []
+
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      #ハッシュロケット
+      #{:sample => 'value'}　　
+      #シンボル型
+      #{sample: 'value'}
+
+      #days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      #days = { month: '(@todays_date + x).month',  date: '(@todays_date+x).day', plans: 'today_plans'}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans }
       @week_days.push(days)
     end
 
