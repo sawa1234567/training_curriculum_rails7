956d170a7e616030b045d969aad607c0bdd531f5
sawa1234567 <ss.sawa80@gmail.com>
2024-05-01 15:46:41 +0900

    Reapply "Merge pull request #14 from sawa1234567/second_training"
    This reverts commit fc90d0aeb0a98268ea29d805106fe3ff17524662.

--- db/migrate/20230603133754_create_plans.rb
+++ db/migrate/20230603133754_create_plans.rb
@@ -1,9 +1,10 @@
 class CreatePlans < ActiveRecord::Migration[7.0]
   def change
     create_table :plans do |t|
-      t.string :plan
-      t.date :date
+      t.string :plan 
+      t.date :date 
       t.timestamps
+
     end
   end
 end
